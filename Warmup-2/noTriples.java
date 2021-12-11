9ac4bb640c7312a302099dc5c46304a61eb25c0e
Zhou Wang <zhouwang2@yahoo.com.au>
2021-12-12 07:46:29 +1100

    Update noTriples.java

--- Warmup-2/noTriples.java
+++ Warmup-2/noTriples.java
@@ -1,7 +1,7 @@
 public boolean noTriples(int[] nums) {
   
-  for (int i = 0; i < nums.length - 2; i++) {
-    if (nums[i+1]==nums[i] && nums[i+2]==nums[i]){
+  for(int i = 0; i < nums.length - 2; i++) {
+    if(nums[i+1] == nums[i] && nums[i+2] == nums[i]){
       return false;
     }
   }
