148240ae13d98b7e2fc0723593ad7190997163a1
Zhou Wang <zhouwang2@yahoo.com.au>
2021-12-12 12:24:19 +1100

    Create countEvens.java

---
+++ Array-2/countEvens.java
@@ -0,0 +1,12 @@
+public int countEvens(int[] nums) {
+  
+  int result = 0;
+  
+  for(int i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 0){
+      result++;
+    }
+  }
+  return result;
+  
+}
